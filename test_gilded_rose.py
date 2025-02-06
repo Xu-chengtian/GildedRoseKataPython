0fa42bd1ee66cd6a2afde0cb627b3a0482f47662
Xu-chengtian <xuchengtian0702@163.com>
2025-02-05 18:34:18 -0800

    add 5 failed test cases in test_gilded_rose.py, 3 for logic and 2 for syntax.

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -5,11 +5,62 @@ from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
+    # example of test that checks for logical errors
+    def test_sulfuras_should_not_decrease_quality(self):
+        items = [Item("Sulfuras", 5, 80)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        sulfuras_item = items[0]
+        self.assertEquals(80, sulfuras_item.quality)
+        self.assertEquals(4, sulfuras_item.sell_in)
+        self.assertEquals("Sulfuras", sulfuras_item.name)
+
+    # example of test that checks for syntax errors
+    def test_gilded_rose_list_all_items(self):
+        items = [Item("Sulfuras", 5, 80)]
+        gilded_rose = GildedRose(items)
+        all_items = gilded_rose.get_items()
+        self.assertEquals(["Sulfuras"], all_items)
+
+    # Logical Error 1
+    def test_aged_brie_increase_quality(self):
+        items = [Item("Luxury Aged Brie", 5, 10)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(items[0].quality, 11)
+        self.assertEqual(items[0].sell_in, 4)
+
+    # Logical Error 2
+    def test_backstage_passes_increase_in_10_days(self):
+        items = [Item("Backstage passes", 8, 40)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(items[0].quality, 42)
+        self.assertEqual(items[0].sell_in, 7)
+
+    # Logical Error 3
+    def test_backstage_passes_increase_in_5_days(self):
+        items = [Item("Backstage passes", 2, 47)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(items[0].quality, 50)
+        self.assertEqual(items[0].sell_in, 1)
+
+    # Syntax Error 1
+    def test_update_function(self):
+        items = [Item("Sulfuras, Hand of Ragnaros", 10, 80)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update()
+        self.assertEqual(items[0].quality, 80)
+        self.assertEqual(items[0].sell_in, 10)
+        
+    # Syntax Error 5
+    def test_get_money(self):
+        items = [Item("Backstage passes", 2, 47)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(items[0].money, 50)
+        self.assertEqual(items[0].sell_in, 1)
 
 
 if __name__ == '__main__':
